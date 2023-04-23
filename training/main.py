de8c5f180747340a23b8c49666b9eb592da9c62d
Himanshu Patil <hipatil1998@gmail.com>
2023-04-22 22:17:23 -0400

    Modified training pipeline, included validation

--- training/main.py
+++ training/main.py
@@ -1,28 +1,42 @@
 import argparse
 import torch
-from training import train
+from training import train, validate
 import pandas as pd
 from dataloader import get_dataloader
-from network import AudioClassifier
+from network import AudioClassifier, TallSkinny, TallSkinny_2
 import sys
+import torch.nn as nn
+import torch.optim as optim
+from tqdm.auto import tqdm
+import wandb
+
 sys.path.append('/home/himanshu/STFU/')
 # print(sys.path)
 
 def main(args):
-    trainloader = get_dataloader(args.data, args.batch_size, args.n_workers, args.input_time_steps, args.right_trim_time_steps)
+    trainloader, valloader = get_dataloader(args.data, args.batch_size, args.n_workers, args.input_time_steps, args.right_trim_time_steps)
 
-    model = AudioClassifier().to(args.device)
+    model = TallSkinny_2().to(args.device)
+    criterion = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
 
-    train(model, trainloader, args)
+    best_acc = 0.0
+    for epoch in range(1, args.n_epochs+1):
+        args.ep = epoch
+        loss = train(args, model, trainloader, criterion, optimizer)
+        acc = validate(args, model, valloader)
+        wandb.log({'Loss': loss, 'Accuracy': acc}, step = args.ep)
+        torch.save(model.state_dict(), 'models/'+'model.pt')
+
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='STFU model training')
     parser.add_argument('--data', type=str, default='Data', help='Data Directory')
-    parser.add_argument('--epochs', type=int, default=10, help='Number of epochs to train')
+    parser.add_argument('--n_epochs', type=int, default=10, help='Number of epochs to train')
     parser.add_argument('--batch_size', type=int, default=16, help='Batch size')
     parser.add_argument('--n_workers', type=int, default=16, help='Number of workers')
-    parser.add_argument('--lr', type=float, default=0.001, help='Learning rate')
+    parser.add_argument('--lr', type=float, default=0.0001, help='Learning rate')
     parser.add_argument('--momentum', type=float, default=0.9, help='Momentum')
     parser.add_argument('--weight_decay', type=float, default=0.0001, help='Weight decay')
     parser.add_argument('--log_interval', type=int, default=10, help='Log interval')
@@ -33,7 +47,12 @@ if __name__ == '__main__':
     parser.add_argument('--save_model_name', type=str, default='model', help='Name to save model')
     parser.add_argument('--input_time_steps', type=int, default=100, help='Number of time steps to use as input')
     parser.add_argument('--right_trim_time_steps', type=int, default=1, help='Number of time steps to trim off the end of the spectrogram')
-    
+    parser.add_argument('--identifier', type=str, default='tall_skinny', help='Identifier for wandb')
+    parser.add_argument('--wandb', action='store_true', default=False, help='Use wandb')
     args = parser.parse_args()
     args.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+    mode = 'online' if args.wandb else 'disabled'
+    wandb.init(project="STFU", name = f'{args.identifier}', reinit=True, config=args)
     main(args)
--- training/training.py
+++ training/training.py
@@ -3,33 +3,50 @@ import torch.nn as nn
 import torch.optim as optim
 from tqdm.auto import tqdm
 
-def train(model, trainloader, args):
-    criterion = nn.BCEWithLogitsLoss()
-    optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=5, verbose=True)
+def train(args, model, trainloader, criterion, optimizer):
+    model.train()
 
-    for n_ep, epoch in enumerate(range(args.epochs)):
-        running_loss = 0.0
-        correct_predictions = 0
-        total_predictions = 0
+    running_loss = 0.0
+    tq = tqdm(trainloader, position=0, leave=True, ascii=True)
+    for i, data in enumerate(tq):
+        # get the inputs and labels and put them on the GPU
+        inputs, labels = data[0].to(args.device), data[1].to(args.device)
+        # ANY Prepocessing of input and labels goes here
+        labels = labels.float()
 
-        tq = tqdm(trainloader, position=0, leave=True, ascii=True)
-        for i, data in enumerate(tq):
-            # get the inputs and labels and put them on the GPU
+        output = model(inputs)
+
+        # print(output, labels.shape)
+        loss = criterion(torch.squeeze(output), labels)
+        tq.set_description("L %.4f" %loss.item())
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        running_loss += loss.item()
+    print(f'Epoch: {args.ep}, Loss: {running_loss/len(trainloader)}')
+    return running_loss/len(trainloader)
+
+
+def validate(args, model, valloader):
+    model.eval()
+    start_test = True
+    with torch.no_grad():
+        for i, data in enumerate(valloader):
             inputs, labels = data[0].to(args.device), data[1].to(args.device)
-            # ANY Prepocessing of input and labels goes here
             labels = labels.float().unsqueeze(1)
-
             output = model(inputs)
-            # print(output, labels.shape)
-            loss = criterion(output, labels)
-            tq.set_description("L %.4f" %loss.item())
-            loss.backward()
-            optimizer.step()
-            optimizer.zero_grad()
+            if start_test:
+                all_output = torch.sigmoid(output).float()
+                all_label = labels.squeeze().float()
+                start_test = False
+            else:
+                all_output = torch.cat((all_output, torch.sigmoid(output).float()), 0)
+                all_label = torch.cat((all_label, labels.squeeze().float()), 0)
 
-            running_loss += loss.item()
-        print(f'Epoch: {n_ep}, Loss: {running_loss}')
+    predicted = (all_output > 0.5).float()
+    accuracy = 100.0*torch.sum(torch.squeeze(predicted).float() == all_label).item() / float(all_label.size()[0])
 
-        torch.save(model.state_dict(), 'models/'+'model.pt')
+    print(f'Validation Accuracy: {accuracy}')
+    return accuracy
+        
 
