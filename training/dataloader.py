b6f619d664d5f385056b9cbae6854b226bfd358d
dallingordon <dallingordon@gmail.com>
2023-04-24 22:15:31 -0400

    fine tuning data loader

--- training/dataloader.py
+++ training/dataloader.py
@@ -82,6 +82,47 @@ class CustomDatasetRandom(Dataset):
         data = torch.from_numpy(spectrogram[:,-self.input_time_steps:]) #the value for time steps is the last x.  20 time steps means last 20
         return data.unsqueeze(0), label
     
+    
+class CustomDatasetFineTune(Dataset):
+    def __init__(self, path, input_time_steps,right_trim_time_steps):
+        self.path = path
+        self.audio_list = os.listdir(self.path)
+        #True.wav, False.wav
+        self.input_time_steps = input_time_steps
+        self.right_trim_time_steps = right_trim_time_steps
+        
+    def __len__(self):
+        return len(self.audio_list) #works
+    
+    def __getitem__(self, idx):
+        
+        file_name = self.audio_list[idx] #has 0 and 1
+        label = file_name.split('.')[0]
+        
+        label = 1 if label=='True' else 0
+        
+ 
+        sample_rate, samples = wavfile.read(os.path.join(self.path, file_name))
+        _, _, spectrogram = signal.spectrogram(samples, sample_rate)
+        
+      
+        spec_height,spec_t = spectrogram.shape
+        trim_idx = np.randint(low = 0, high = spec_t - self.input_time_steps)
+        
+        if spec_t < self.input_time_steps:
+            pad_width = self.input_time_steps - spec_t 
+            pad_zeros = np.zeros((spec_height,pad_width), dtype=np.float32)
+            # pad_zeros.shape
+            spectrogram = np.concatenate([pad_zeros,spectrogram], axis = 1)
+        
+        data = torch.from_numpy(spectrogram[:,trim_idx:trim_idx+self.input_time_steps]) #the value for time steps is the last x.  20 time steps means last 20
+        data = data/data.max()
+        return data.unsqueeze(0), label
+    
+
+    
+
+    
 def get_dataloader(path, batch_size, num_workers, input_time_steps,right_trim_time_steps):
     dataset = CustomDataset(path,input_time_steps,right_trim_time_steps)
     train_size = int(0.8*len(dataset))
